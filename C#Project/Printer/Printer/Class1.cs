7cb70ccc3e8e486ac0f02cc3f724a4ac899b472d
hxdyj <951540966@qq.com>
2021-12-09 21:20:31 +0800

    add c# project

---
+++ C#Project/Printer/Printer/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.Threading.Tasks;
+
+namespace Win
+{
+    public class Printer
+    {
+        PrintDocument pd = new PrintDocument();
+        public async Task<Object> getPrinters(object input)
+        {
+            return PrinterSettings.InstalledPrinters;
+        }
+
+        public async Task<Object> getPaperSizes(object printerName)
+        {
+            pd.PrinterSettings.PrinterName = printerName.ToString();
+            return pd.PrinterSettings.PaperSizes;
+        }
+
+        public async Task<Object> getTrays(object printerName)
+        {
+            pd.PrinterSettings.PrinterName = printerName.ToString();
+            return pd.PrinterSettings.PaperSources;
+        }
+
+    }
+}
